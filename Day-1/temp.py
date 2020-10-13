8260e0c15396d48be520ab7da94d80a8613c4de2
sipsmehta <69897673+sipsmehta@users.noreply.github.com>
2020-10-13 18:16:04 +0530

    Sparsh Mehta
    Temperature conversion

---
+++ Day-1/temp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+temp = int(input("enter value of temp in faherenheit"))
+cel = ((int(temp) - 32)*5)/9
+print('temprature in celcius is :' , cel)
+
+
+# In[ ]:
+
+
+
+
